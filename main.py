3b53e1d3ba420034abcf44d3ab61828e98abcb83
ArtemGribov <gribov.ag@gmail.com>
2023-08-02 12:49:33 +0300

    Финальная версия

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+#175
 #Создаем граф
 graph = {}
 #Добавляем узел "start"
@@ -7,14 +7,12 @@ graph["start"] = {}
 graph["start"]["a"] = 6
 graph["start"]["b"] = 2
 
-
 #Просмотр веса ребра по двум узлам
-print(graph["start"]["a"])
+#print(graph["start"]["a"])
 #Просмотр всех соседних узлов
-print(graph["start"].keys())
+#print(graph["start"].keys())
 #Просмотр всех соседних узлов и с весами
-print(graph["start"].items())
-
+#print(graph["start"].items())
 
 #Добавляем остальные узлы с соседями
 graph["a"] = {}
@@ -23,7 +21,6 @@ graph["b"] = {}
 graph["b"]["a"] = 3
 graph["b"]["fin"] = 5
 graph["fin"] = {}
-
 
 #Таблица стоимостей
 infinity = float("inf") #бесконечность
@@ -40,3 +37,29 @@ parents["in"] = None
 
 #Массив для отслеживания всех уже обработанных узлов
 processed = []
+
+#Алгоритм Дейкстры
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+#Проверка
+print(costs)
